45424b033a0ce6cafa3d2397a1e6cc932201ce70
katharinasch <ms.schreiber.katharina@gmail..com>
2021-09-15 16:31:38 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react'
+
 
 function App() {
+  const [value, setValue] = useState(() => {
+    const saved = localStorage.getItem("value");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  const [agreed, setAgreed] = useState(())
+
+
+  useEffect(() => {
+    // This effect uses the `value` variable,
+    // so it "depends on" `value`.
+    console.log(value);
+    localStorage.setItem("value", JSON.stringify(value));
+  }, [value])  // pass `value` as a dependency
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="mb-3 pt-0">
+        <input value={value} onChange={e => setValue(e.target.value)} type="text" placeholder="Placeholder" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm text-black border-0 shadow outline-none focus:outline-none focus:ring w-full"/>
+      </div>
+
       </header>
     </div>
   );
